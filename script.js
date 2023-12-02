36d7c15ddaf412f854b084957d3b1620f739af09
Toni Mertanen <t.mertanen@hotmail.com>
2023-12-02 22:23:13 +0200

    Skeleton of game ready - gamePlay function needs fixing

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-//Creating audio objects for each instrument and preloading them
+//Function for creating audio objects for each button and preloading them
 function createAudioObject(soundName) {
     let audio = new Audio();
     audio.src = `sounds/${soundName}.mp3`;
     audio.preload = "auto";
     return audio;
 }
-
-/*let blue = createAudioObject("blue");
-let green = createAudioObject("green");
-let red = createAudioObject("red");
-let yellow = createAudioObject("yellow");
-let wrong = createAudioObject("wrong");*/
 
 
 //Assigning variables for the game
@@ -21,28 +15,59 @@ let highScore = 0;
 let correctAnswer = true;
 
 
+//Assigning the click eventlisteners to elements
+let buttons = ["blue", "green", "red", "yellow"];
+buttons.forEach((button) => {
+    $(`#${button}`).click(() => {
+        $("h1").text(`Score: ${playerScore}`);
+        if (buttonQueue.length === 0 || button != buttonQueue[0]) {
+            createAudioObject("wrong").play();
+            playerScore > highScore ? highScore = playerScore : highScore = highScore;
+            correctAnswer = false;
+            $("h1").text(`Score: ${playerScore} HiScore: ${highScore}`);
+            setTimeout(() => {
+                playerScore = 0;
+                buttonQueue = [];
+                timer = 200;
+                $("h1").text(`Press A Key to Start`);
+            }, 1000);
+        } else {
+            createAudioObject(button).play();
+            buttonQueue.shift();
+            playerScore++;
+        }
+    });
+});
+
+
 //Creating the game sequence
 function randomButton() {
     let list = ["blue", "green", "red", "yellow"];
     return list[Math.floor(Math.random() * list.length)];
 }
 
-
 function gamePlay() {
-    while (correctAnswer) {
-        
+    if (correctAnswer) {
+        setTimeout(() => {
+            let button = randomButton();
+            buttonQueue.push(button);
+            $(`#${button}`).fadeOut(400).fadeIn(400);
+            createAudioObject(button).play();
+            timer -= 5;
+
+            if (timer <= 30) {
+                timer = 30;
+                gamePlay(); // Call gamePlay recursively to continue the sequence
+            } else {
+                gamePlay();
+            }
+        }, timer);
     }
 }
 
 
 //Starting the game
-$(document).keydown( () => gamePlay() );
-  
-
-//Assigning the click eventlisteners to elements
-let buttons = ["blue", "green", "red", "yellow"];
-buttons.forEach((button) => {
-    $(`#${button}`).click(() => {
-        createAudioObject(button).play();
-    })
+$(document).keydown( () => { 
+    correctAnswer = true;
+    gamePlay();
 });
