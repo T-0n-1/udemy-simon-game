3524415747f3ae12f0d7f2739bd2d3e464c9a830
Toni Mertanen <t.mertanen@hotmail.com>
2023-12-02 18:50:12 +0200

    Fixed bug in createAudioObject function

---
+++ script.js
@@ -0,0 +1,45 @@
+//Creating audio objects for each instrument and preloading them
+function createAudioObject(soundName) {
+    let audio = new Audio();
+    audio.src = `sounds/${soundName}.mp3`;
+    audio.preload = "auto";
+    return audio;
+}
+
+let blue = createAudioObject("blue");
+let green = createAudioObject("green");
+let red = createAudioObject("red");
+let yellow = createAudioObject("yellow");
+let wrong = createAudioObject("wrong");
+
+
+//Assigning variables for the game
+let timer = 200;
+let buttonQueue = [];
+let playerScore = 0;
+let highScore = 0;
+
+
+//Creating the game sequence
+function randomButton() {
+    let list = ["blue", "green", "red", "yellow"];
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+
+function gamePlay() {
+    
+}
+
+
+//Starting the game
+$(document).keydown( () => gamePlay() );
+  
+
+//Assigning the click eventlisteners to elements
+let buttons = ["blue", "green", "red", "yellow"];
+buttons.forEach((button) => {
+    $(`#${button}`).click(() => {
+        playSound(button);
+    })
+});
